26266deede33095602254c5fef50c704f7658fb9
arnaumarifrances <arnaumarifrances@gmail.com>
2025-07-17 18:35:15 +0200

    Initial commit - Spring Boot InOut project

---
+++ src/test/java/com/inout/InOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
